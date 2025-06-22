0df1500c85d9d801a6e4b316a77413851cb57dd2
tgartside <128266784+tgartside@users.noreply.github.com>
2025-06-22 14:27:52 -0700

    First iteration of menus added
    Title screen and pause menu added. Pause menu does not properly work yet, as mouse is not available. Probably due to first person camera

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -2,19 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
 
+    [Header("Object References")]
+    public GameObject pauseMenu;
+
     private void Start()
     {
         Application.targetFrameRate = 144;
+        if (SceneManager.GetActiveScene().name.Equals("TerrainTesting"))
+        {
+            StartCoroutine(FadeIn());
+        }
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!SceneManager.GetActiveScene().name.Equals("TitleScreen"))
         {
-            Application.Quit();
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (pauseMenu.activeSelf)
+                {
+                    Unpause();
+                }
+
+                else
+                {
+                    pauseMenu.SetActive(true);
+                    Time.timeScale = 0f;
+                }
+            }
         }
+    }
+
+    public void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToTitle()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    IEnumerator FadeIn()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFade>().StartFadeIn();
+        yield return new WaitForSeconds(1);
+    }
+
+    IEnumerator FadeOut()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFade>().StartFadeOut();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("TerrainTesting");
     }
 }
